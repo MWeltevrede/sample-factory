031a4c9b5d4effb53097f66efd9fd0c006233bef
Caroline Horsch <carolinehorsch@gmail.com>
2025-02-07 11:19:31 +0100

    Added containerization

---
+++ run_vizdoom_container.sh
@@ -0,0 +1,6 @@
+apptainer exec --nv vizdoom.sif \
+     /bin/bash -c "\
+     python -m sample_factory.launcher.run \
+          --run=sf_examples.vizdoom.experiments.doom_battle_contexts\
+          --backend=processes --num_gpus=1 --max_parallel=4\
+               --pause_between=0 --experiments_per_gpu=2"
